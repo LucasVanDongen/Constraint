75c19dd748ba29efd0dbbd8af186e1de8ad5f556
Lucas van Dongen <lucas.van.dongen@gmail.com>
2019-03-26 22:50:07 -0300

    Documented existing UIView extension functions

--- Classes/UIView+Constraint.swift
+++ Classes/UIView+Constraint.swift
@@ -38,6 +38,15 @@ extension UIView {
     }
 
     @discardableResult
+    /**
+     Adds layouts that determine the offset of this view towards it's parent view
+
+     - parameters:
+       - offset: The amount of points this view will be inset in it's parentView
+       - respectingLayoutGuides: Default it will attach to the edges of the parent view (value `false`). If you want it to attach to the layoutGuides for example to not be covered by the notch this should be set to `true`
+
+     - Returns: a reference of the view you call this function on to allow chaining
+     */
     public func attach(offset: CGFloat = 0, respectingLayoutGuides: Bool = false) -> Self {
         guard let superview = superview else {
             assertionFailure("This view should already have a superview")
@@ -78,21 +87,62 @@ extension UIView {
     }
 
     @discardableResult
+    /**
+     Adds layouts that determine the offset of this view towards it's parent view
+
+     - parameters:
+     - offset: An Offsetable value, like `0.orMore`
+
+     - Returns: a reference of the view you call this function on to allow chaining
+     */
     public func attach(_ offset: Offsetable = 0) -> Self {
         return attach(top: offset, leading: offset, bottom: offset, trailing: offset)
     }
 
     @discardableResult
+    /**
+     Adds layouts that determine the offset of this view towards it's parent view.
+     This version is useful to copy `UIEdgeInsets` to real constraints.
+
+     Note: it will map `left` to `leading` and `right` to `trailing`
+
+     - parameters:
+     - insets: `UIEdgeInsets` determining all of the insets
+
+     - Returns: a reference of the view you call this function on to allow chaining
+     */
     public func attach(with insets: UIEdgeInsets) -> Self {
         return attach(top: insets.top, leading: insets.left, bottom: insets.bottom, trailing: insets.right)
     }
 
     @discardableResult
+    /**
+     Adds layouts that determine the offset of this view towards it's parent view
+
+     This is a convenience function
+
+     - parameters:
+     - vertically: The offset that should be added at the `top` and `bottom` edge
+     - horizontally: The offst that should be added at the `leading` and `trailing` edge
+
+     - Returns: a reference of the view you call this function on to allow chaining
+     */
     public func attach(vertically: Offsetable? = nil,
                        horizontally: Offsetable? = nil) -> Self {
         return attach(top: vertically, leading: horizontally, bottom: vertically, trailing: horizontally)
     }
 
+    /**
+     Adds layouts that determine the offset of this view towards it's parent view.
+
+     This is a convenience function for giving the same offset to one or more sides in one line
+
+     - parameters:
+     - sides: Declare what sides should be included
+     - offset: The amount of offset in Offsetable format, for example 8.orMore
+
+     - Returns: a reference of the view you call this function on to allow chaining
+     */
     @discardableResult
     public func attach(sides: Set<Side>,
                        _ offset: Offsetable = 0) -> Self {
@@ -117,6 +167,17 @@ extension UIView {
         return attach(top: top, leading: leading, bottom: bottom, trailing: trailing)
     }
 
+    /**
+     Adds layouts that determine the offset of this view towards it's parent view
+
+     - parameters:
+     - top: The amount of points this view will be inset from the top edge of it's parentView, for example 0.orMore
+     - leading: The amount of points this view will be inset from the leading edge of it's parentView, for example 0.orMore
+     - bottom: The amount of points this view will be inset from the bottom edge of it's parentView, for example 0.orMore
+     - trailing: The amount of points this view will be inset from the trailing edge of it's parentView, for example 0.orMore
+
+     - Returns: a reference of the view you call this function on to allow chaining
+     */
     @discardableResult
     public func attach(top: Offsetable? = nil,
                        leading: Offsetable? = nil,
@@ -164,6 +225,16 @@ extension UIView {
     }
 
     @discardableResult
+    /**
+     Adds layouts that centers the view on one or more axis of it's parent view
+
+     - parameters:
+     - axis: Can be `.x`, `.y` or `.both`
+     - adjusted: The amount of points the center is shifted, a positive value means it's moving towards the trailing and/or bottom edge and a negative value will shift it top the top and/or leading edge
+     - priority: The priority given to these constraints
+
+     - Returns: a reference of the view you call this function on to allow chaining
+     */
     public func center(axis: CenterAxis = .both,
                        adjusted: CGFloat = 0.0,
                        priority: UILayoutPriority = UILayoutPriority.required) -> Self {
@@ -178,6 +249,19 @@ extension UIView {
     }
 
     @discardableResult
+    /**
+     Adds layouts that centers the view to one or more axis of another view
+
+     Note: Use the `center` function if you want to center this view inside of it's parent view
+
+     - parameters:
+     - axis: Can be `.x`, `.y` or `.both`
+     - viewToAlignWith: The view you want to align it with
+     - adjusted: The amount of points the center is shifted, a positive value means it's moving towards the trailing and/or bottom edge and a negative value will shift it top the top and/or leading edge
+     - priority: The priority given to these constraints
+
+     - Returns: a reference of the view you call this function on to allow chaining
+     */
     public func align(axis: CenterAxis,
                       to viewToAlignWith: UIView,
                       adjusted adjustment: CGFloat = 0.0,
@@ -199,12 +283,37 @@ extension UIView {
     }
 
     @discardableResult
+    /**
+     Adds layouts that adds a space between this view and another view
+
+     - parameters:
+     - direction: The direction where to space to, this can be `.below`, `.trailing`, `.above` or `.leading
+     - view: The other view you're spacing off
+     - relation: This can be `.exactly`, `.orLess` or `.orMore`
+     - priority: The priority given to these constraints
+
+     - Returns: a reference of the view you call this function on to allow chaining
+     */
     public func space(_ direction: SpaceDirection,
                       _ view: UIView,
                       _ relation: Relation = .exactly,
                       priority: UILayoutPriority = UILayoutPriority.required) -> Self {
         return space(0, direction, view, relation, priority: priority)
     }
+
+    @discardableResult
+    /**
+     Adds layouts that adds a space between this view and another view
+
+     - parameters:
+     - distance: The amount of points of distance between this view and the other view. A positive value will create a gap, a negative value will make the overlap
+     - direction: The direction where to space to, this can be `.below`, `.trailing`, `.above` or `.leading
+     - view: The other view you're spacing off
+     - relation: This can be `.exactly`, `.orLess` or `.orMore`
+     - priority: The priority given to these constraints
+
+     - Returns: a reference of the view you call this function on to allow chaining
+     */
     public func space(_ distance: CGFloat,
                       _ direction: SpaceDirection,
                       _ view: UIView,
@@ -238,6 +347,20 @@ extension UIView {
     }
 
     @discardableResult
+    /**
+     Aligns one edge of this view with the same edge of another view.
+
+     Note: use `attach` when you want to align the sides with the super view
+
+     - parameters:
+     - side: Can be `.top`, `.leading`, `.bottom` or `.trailing`
+     - distance: The amount of points of space between the
+     - viewToAlignWith: The view you want to align it with
+     - adjusted: The amount of points the center is shifted, a positive value means it's moving towards the trailing and/or bottom edge and a negative value will shift it top the top and/or leading edge
+     - priority: The priority given to these constraints
+
+     - Returns: a reference of the view you call this function on to allow chaining
+     */
     public func align(_ side: Side,
                       _ distance: CGFloat = 0,
                       to viewToAlignTo: UIView) -> Self {
@@ -251,6 +374,18 @@ extension UIView {
     }
 
     @discardableResult
+    /**
+     Aligns multiple edges of this view with the same edges of another view.
+
+     Note: use `attach` when you want to align the sides with the super view
+
+     - parameters:
+     - sides: Can be `.top`, `.leading`, `.bottom` and/or `.trailing`
+     - distance: The amount of points of space between the
+     - viewToAlignWith: The view you want to align it with
+
+     - Returns: a reference of the view you call this function on to allow chaining
+     */
     public func align(_ sides: Set<Side>,
                       _ distance: CGFloat = 0,
                       to viewToAlignTo: UIView) -> Self {
@@ -267,6 +402,19 @@ extension UIView {
     }
 
     @discardableResult
+    /**
+     Aligns multiple edges of this view with the same edges of another view.
+
+     Note: use `attach` when you want to align the sides with the super view
+     ToDo: this seems a duplicate of the function above
+
+     - parameters:
+     - sides: Can be `.top`, `.leading`, `.bottom` and/or `.trailing`
+     - distance: The amount of points of space between the
+     - viewToAlignWith: The view you want to align it with
+
+     - Returns: a reference of the view you call this function on to allow chaining
+     */
     public func align(sides: Set<Side>,
                       _ distance: CGFloat = 0,
                       to viewToAlignTo: UIView) -> Self {
@@ -280,6 +428,18 @@ extension UIView {
     }
 
     @discardableResult
+    /**
+     Defines a ratio between widht and height for this view
+
+     Note: this a convenience function allowing you to turn the size of for example a `UIImage` into constraints allowing it to scale without losing it's ratio
+
+     - parameters:
+     - ratio: contains width and height
+     - relation: This can be `.exactly`, `.orLess` or `.orMore`
+     - priority: The priority given to these constraints
+
+     - Returns: a reference of the view you call this function on to allow chaining
+     */
     public func ratio(_ ratio: CGSize,
                       _ relation: Relation = .exactly,
                       priority: UILayoutPriority = UILayoutPriority.required) -> Self {
@@ -289,6 +449,17 @@ extension UIView {
     }
 
     @discardableResult
+    /**
+     Defines a ratio between widht and height for this view
+
+     - parameters:
+     - width: The ratio of the width. If this would be 2.0 and the height would be 1.0 it would be twice as wide as it is tall
+     - height: The ratio of the height. If this would be 2.0 and the width would be 1.0 it would be twice as tall as it is wide
+     - relation: This can be `.exactly`, `.orLess` or `.orMore`
+     - priority: The priority given to these constraints
+
+     - Returns: a reference of the view you call this function on to allow chaining
+     */
     public func ratio(of width: CGFloat,
                       to height: CGFloat = 1,
                       _ relation: Relation = .exactly,
@@ -299,9 +470,22 @@ extension UIView {
     }
 
     @discardableResult
+    /**
+     Allows you to relate the height of this view to the height another view
+
+     - parameters:
+     - otherView: The view to relate to
+     - multiplied: The multiplier applied to it's size. For example with a multiplier of 2.0 the height of the other view would be doubled
+     - adjusted: It's possible to shift the multiplied value with a fixed value
+     - relation: This can be `.exactly`, `.orLess` or `.orMore`
+     - priority: The priority given to these constraints
+
+     - Returns: a reference of the view you call this function on to allow chaining
+     */
     public func height(relatedTo otherView: UIView,
                        multiplied multiplier: CGFloat = 1.0,
                        adjusted adjustment: CGFloat = 0.0,
+                       _ relation: Relation = .exactly,
                        priority: UILayoutPriority = UILayoutPriority.required) -> Self {
         guard let viewToAddTo = try? Constraint.determineSharedSuperview(between: self, and: otherView) else {
             assertionFailure("They should share a node")
@@ -311,6 +495,7 @@ extension UIView {
         viewToAddTo.addConstraint(Constraint.height(of: self,
                                                     relatedTo: otherView,
                                                     multiplied: multiplier,
+                                                    priority: priority,
                                                     adjusted: adjustment,
                                                     relation))
 
@@ -318,6 +503,17 @@ extension UIView {
     }
 
     @discardableResult
+    /**
+     Allows you to relate the width of this view to the width of another view
+
+     - parameters:
+     - otherView: The view to relate to
+     - multiplied: The multiplier applied to it's size. For example with a multiplier of 2.0 the width of the other view would be doubled
+     - adjusted: It's possible to shift the multiplied value with a fixed value
+     - priority: The priority given to these constraints
+
+     - Returns: a reference of the view you call this function on to allow chaining
+     */
     public func width(relatedTo otherView: UIView,
                       multiplied multiplier: CGFloat = 1.0,
                       priority: UILayoutPriority = UILayoutPriority.required,
@@ -339,6 +535,18 @@ extension UIView {
     }
 
     @discardableResult
+    /**
+     Allows you to set the width and height of the view.
+
+     Note: it's a shorthand function for `width` and `height` used to set the frame size to real constraints
+
+     - parameters:
+     - size: The size you want to set
+     - widthRelation: This can be `.exactly`, `.orLess` or `.orMore`
+     - heightRelation: This can be `.exactly`, `.orLess` or `.orMore`
+
+     - Returns: a reference of the view you call this function on to allow chaining
+     */
     public func size(_ size: CGSize,
                      widthRelation: Relation = .exactly,
                      heightRelation: Relation = .exactly) -> Self {
@@ -351,6 +559,19 @@ extension UIView {
     }
 
     @discardableResult
+    /**
+     Allows you to set the width and height of the view.
+
+     Note: it's a shorthand function for `width` and `height` used to set the frame size to real constraints
+
+     - parameters:
+     - width: The width you want to set
+     - widthRelation: This can be `.exactly`, `.orLess` or `.orMore`
+     - height: The height you want to set
+     - heightRelation: This can be `.exactly`, `.orLess` or `.orMore`
+
+     - Returns: a reference of the view you call this function on to allow chaining
+     */
     public func size(width: CGFloat,
                      _ widthRelation: Relation = .exactly,
                      height: CGFloat,
@@ -364,6 +585,35 @@ extension UIView {
     }
 
     @discardableResult
+    public func size(relatedTo otherView: UIView,
+                     multiplied multiplier: CGFloat = 1.0,
+                     priority: UILayoutPriority = UILayoutPriority.required,
+                     adjusted adjustment: CGFloat = 0.0,
+                     _ relation: Relation = .exactly) -> UIView {
+        guard let viewToAddTo = try? Constraint.determineSharedSuperview(between: self, and: otherView) else {
+            assertionFailure("Either one of them should be the parent")
+            return self
+        }
+
+        viewToAddTo.addConstraints([
+            Constraint.width(of: self, relatedTo: otherView, multiplied: multiplier, priority: priority, adjusted: adjustment, relation),
+            Constraint.height(of: self, relatedTo: otherView, multiplied: multiplier, priority: priority, adjusted: adjustment, relation)
+            ])
+
+        return self
+    }
+
+    @discardableResult
+    /**
+     Allows you to set the width of the view.
+
+     - parameters:
+     - width: The width you want to set
+     - relation: This can be `.exactly`, `.orLess` or `.orMore`
+     - priority: The priority given to these constraints
+
+     - Returns: a reference of the view you call this function on to allow chaining
+     */
     public func width(_ width: CGFloat,
                       _ relation: Relation = .exactly,
                       priority: UILayoutPriority = UILayoutPriority.required) -> Self {
@@ -375,6 +625,16 @@ extension UIView {
     }
 
     @discardableResult
+    /**
+     Allows you to set the height of the view.
+
+     - parameters:
+     - height: The width you want to set
+     - relation: This can be `.exactly`, `.orLess` or `.orMore`
+     - priority: The priority given to these constraints
+
+     - Returns: a reference of the view you call this function on to allow chaining
+     */
     public func height(_ height: CGFloat,
                        _ relation: Relation = .exactly,
                        priority: UILayoutPriority = UILayoutPriority.required) -> Self {

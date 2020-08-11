d87939bf9f92a9bb0170ae3986883cfebb330729
Lucas van Dongen <lucas.van.dongen@gmail.com>
2020-08-11 20:50:27 +0200

    Added chaining function for adding a view to a UIStackView

--- Classes/UIView+Constraint.swift
+++ Classes/UIView+Constraint.swift
@@ -25,6 +25,20 @@ extension UIView {
     }
 
     @discardableResult
+    /**
+     Adds the UIView as a subview to the view you specify
+
+     - parameters:
+     - stackView: the `UIStackView` to add this view as a subview to
+
+     - Returns: a reference of the view you call this function on to allow chaining
+     */
+    public func addedAsArrangedSubview(to stackView: UIStackView) -> Self {
+        stackView.addArrangedSubview(self)
+        return self
+    }
+
+    @discardableResult
     @available(*, deprecated, renamed: "attach()", message: "Replaced by a simpler version of the function attach that does not ask for the parentView anymore")
     public func snap(inside parentView: UIView? = nil,
                      offset: CGFloat = 0) -> Self {
